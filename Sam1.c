c142d9fbbe2f0be2c1129f378892158c03c83e75
RazeenLI <62377495+RazeenLI@users.noreply.github.com>
2020-09-13 16:48:30 +1000

    comp2151 tut week 1 q2

--- Sam1.c
+++ Sam1.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
 
-void giveUp(char *res);
-void printNum(char *string, int from, int end);
 int main(int argc, char **argv)
 {
-    if (argc != 4) giveUp(argv[0]);
-    printNum(argv[1], atoi(argv[2]), atoi(argv[3]));
-    return 0;
-}
-
-void giveUp(char *res)
-{
-    fprintf(stderr, "%s usage:", res);
-}
-
-void printNum(char *string, int from, int end) 
-{
-    int stop = strlen(string);
-    int sign = 0;
-    for (int i = 0; i < end; i++) {
-        printf("%c", string[from]);
-        if (from == stop - 1)
+    int i, j, *a;
+    int N = 0;
+    
+    // initialisation
+    assert(argc > 1);
+    sscanf(argv[1], "%d", &N);
+    assert(N > 0);
+    a = malloc(N * sizeof(int));
+    assert(a != NULL);
+    for (i = 1; i < N; i++) a[i] = 1;
+    
+    // computation
+    for (i = 2; i < N; i++)
+    {
+        if (a[i])
         {
-            sign = 1;
-        } else if (from == 0)
-        {
-            sign = 0;
-        }
-        switch (sign)
-        {
-            case 1:
-                from--;
-                break;
-            case 0:
-                from++;
+            for (j = i; i * j < N; j++) a[i* j] = 0;
         }
     }
-    printf("\n");
+
+    // results
+    for (i = 2; i < N; i++)
+    {
+        if (a[i]) printf("%d\n", i);
+    }
+    exit(EXIT_SUCCESS);
 }
