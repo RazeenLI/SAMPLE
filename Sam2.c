6b0a016a130416feacd61941bd92df61ac1de9d8
RazeenLI <62377495+RazeenLI@users.noreply.github.com>
2020-09-15 15:16:00 +1000

    2521 tut week01 Q6

--- Sam2.c
+++ Sam2.c
@@ -11,18 +11,47 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <errno.h>
-
-int main() {
-    int num1 = 1, num2 = 1;
-    double num3 = 0;
-    do {
-        num3 = num3 + num1 + num2;
-        num3 = num3 / num2;
-        num3 += num1;
-        num3 *= 2;
-        num1++;
-        num2++;
-        printf("%lf\n", num3);
-    } while (num3 < 50);
+struct node {
+    struct node * next;
+    int value;
+};
+struct node * creatArray(struct node * end);
+int sumAll(struct node * array);
+int main(int argc, char *argv[]) 
+{
+    struct node * array1 = creatArray(NULL);
+    struct node * array2 = creatArray(array1);
+    struct node * array3 = creatArray(array2);
+    struct node * array4 = creatArray(array3);
+    struct node * pointer = array4;
+    int i = 0;
+    while (pointer != NULL)
+    {
+        pointer->value = atoi(argv[i + 1]);
+        i++;
+        pointer = pointer->next;
+    }
+    for (pointer = array4; pointer != NULL; pointer = pointer->next) printf(" %d\n", pointer->value);
+    int sum = sumAll(array4);
+    printf("%d\n", sum);
     return 0;
+}
+
+int sumAll(struct node * array)
+{
+    int sum = 0;
+    struct node * pointer = array;
+    for (sum = 0; pointer != NULL; pointer = pointer->next)
+    {
+        sum += pointer->value;
+    }
+    return sum;
+}
+
+struct node * creatArray(struct node * end) 
+{
+    struct node * array = malloc(sizeof(struct node));
+    assert(array != NULL);
+    array->next = end;
+    return array;
 }
