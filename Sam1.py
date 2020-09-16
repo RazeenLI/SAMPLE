328caef70829c774c3590654ad4359948119dd71
RazeenLI <62377495+RazeenLI@users.noreply.github.com>
2020-09-17 08:09:39 +1000

    Sam1.py now is use for print number

--- Sam1.py
+++ Sam1.py
@@ -1,20 +1,7 @@
-import sys
+a = input("please enter a number:")
 
-word = sys.argv[1]
-start = (int)(sys.argv[2])
-end = (int)(sys.argv[3])
-sign = 0
-i = 0
-
-while i < end :
-    print(word[start],end='')
-    if sign :
-        start -= 1
-        if start == 0 :
-            sign = 0
-    else :
-        start += 1
-        if start == len(word) - 1 :
-            sign = 1
-    i += 1
-print()
+if a.isdigit():
+    a = (int)(a)
+    print(a * a)
+else:
+    print("errer")
--- Sam2.c
+++ Sam2.c
@@ -12,46 +12,30 @@
 #include <fcntl.h>
 #include <errno.h>
 struct node {
-    struct node * next;
-    int value;
+    int * next;
+    int size;
 };
-struct node * creatArray(struct node * end);
-int sumAll(struct node * array);
-int main(int argc, char *argv[]) 
-{
-    struct node * array1 = creatArray(NULL);
-    struct node * array2 = creatArray(array1);
-    struct node * array3 = creatArray(array2);
-    struct node * array4 = creatArray(array3);
-    struct node * pointer = array4;
-    int i = 0;
-    while (pointer != NULL)
-    {
-        pointer->value = atoi(argv[i + 1]);
-        i++;
-        pointer = pointer->next;
-    }
-    for (pointer = array4; pointer != NULL; pointer = pointer->next) printf(" %d\n", pointer->value);
-    int sum = sumAll(array4);
-    printf("%d\n", sum);
+
+void swap(struct node * a, int i, int j);
+int main() {
+    struct node * array = malloc(sizeof(struct node) * 4);
+    *(array[0].next) = 0;
+    *(array[1].next) = 1;
+    *(array[2].next) = 2;
+    *(array[3].next) = 3;
+    for (int i = 0; i < 4; i++) printf("%d", *(array[i].next));
+    printf("\n");
+    swap(array, 1, 3);
+    for (int i = 0; i < 4; i++) printf("%d", *(array[i].next));
+    printf("\n");
+
     return 0;
 }
 
-int sumAll(struct node * array)
+void swap(struct node * a, int i, int j)
 {
-    int sum = 0;
-    struct node * pointer = array;
-    for (sum = 0; pointer != NULL; pointer = pointer->next)
-    {
-        sum += pointer->value;
-    }
-    return sum;
-}
-
-struct node * creatArray(struct node * end) 
-{
-    struct node * array = malloc(sizeof(struct node));
-    assert(array != NULL);
-    array->next = end;
-    return array;
+    int * tmp;
+    tmp = a[i].next;
+    a[i].next = a[j].next;
+    a[j].next = tmp;
 }
