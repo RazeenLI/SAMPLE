1af613a63d23daf45880b030c06d49b8117f718f
RazeenLI <62377495+RazeenLI@users.noreply.github.com>
2020-10-09 22:08:19 +0800

    Sam1 for function, Sam2 for HTTP

--- Sam1.py
+++ Sam1.py
@@ -1,3 +1,21 @@
-for x in range(0, 10):
-    print(f"     {x}")
-    print((10*(x**2)+4*x+1)%12)
+result = []
+finish = 7
+T1 = 3
+T2 = 7
+for x in range(0, finish + 1):
+    result.append((4*(x**2)+7*x+7)%8)
+print('set(', end = '')
+for x in range(0, finish + 1):
+    if x == T1 or x == T2:
+        print(str(result[x]) + ',', end='')
+print(')')
+print()
+print('set(', end = '')
+for x in range(0, finish + 1):
+    if result[x] == T1 or result[x] == T2:
+        print(str(x) + ',', end='')
+print(')')
+print()
+for x in range(0, finish + 1):
+    print('x = ' + str(x), end = '\t')
+    print('f(x) = ' + str(result[x]))
--- Sam2.py
+++ Sam2.py
@@ -1,21 +1,30 @@
+from flask import Flask, send_file, request
 import json
+APP = Flask(__name__)
 
-DATA = {
-    'name':
-    [
-        '1',
-        '2',
-        '3',
-        '4',
-    ],
-    'year':
-    [
-        '12',
-        '11',
-        '13',
-        '14',
-    ]
-}
 
-with open('Sam.json', 'w') as FILE:
-    json.dump(DATA, FILE)
+@APP.route("/")
+def hello():
+    return "Hello world"
+
+
+@APP.route("/润泽", methods=['GET'])
+def data1():
+    Razeen = {}
+    Razeen['Name'] = request.args.get('data1')
+    Razeen['age'] = request.args.get('data2')
+    return json.dumps(Razeen)
+
+
+@APP.route("/img")
+def img():
+    return send_file("/Users/eraser_mbp/Pictures/IMG_0372.PNG", mimetype='image/png')
+
+
+@APP.route("/file")
+def file():
+    return send_file(" /Users/eraser_mbp/Documents/UNSW/20T3\ 课程要求.html")
+
+
+if __name__ == "__main__":
+    APP.run(port=5000)
